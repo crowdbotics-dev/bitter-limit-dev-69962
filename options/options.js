1bbaa41dfd0560fae8d15f9df4d8263b34778a07
Crowdbotics <engineering@crowdbotics.com>
2023-03-16 09:44:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-limit-dev-69962",
+  url: "https://bitter-limit-dev-69962.botics.co",
+  api: "https://bitter-limit-dev-69962.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
